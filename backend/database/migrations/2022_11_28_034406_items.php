7a6b18699566d918f3af6a45b36ba51633a799ab
Paing Hein Thu <painghein.thu@frontiir.net>
2022-11-28 14:54:13 +0630

    create some table schema

---
+++ backend/database/migrations/2022_11_28_034406_items.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('category_id');
+            $table->bigInteger('city_id')->default(-1);
+            $table->string('log_number')->default(-1);
+            $table->integer('qty');
+            $table->integer('base_price');
+            $table->string('sellable_currency');
+            $table->dateTime('available_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_categories');
+    }
+};
