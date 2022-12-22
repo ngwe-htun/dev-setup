18d43503e6869504799bde24902c43df16e5572a
omphwa <arpalar.omphwa@gmail.com>
2022-12-22 11:45:31 +0630

    export pdf supported

--- frontend/src/config/app.js
+++ frontend/src/config/app.js
@@ -1,10 +1,10 @@
 
 export const Config = {
-    "host": "http://10.20.0.1",
-    "base_host": "http://10.20.0.1/gold/api/v1",
-    "admin_host" : "http://10.20.0.1/gold/api/v1/admin",
-    "client_host": 'http://10.20.0.1/gold/api/v1/client',
-    "client_token": "abc",
+    "host": "http://mmgold.test",
+    "base_host": "http://mmgold.test/gold/api/v1",
+    "admin_host" : "http://mmgold.test/gold/api/v1/admin",
+    "client_host": 'http://mmgold.test/gold/api/v1/client',
+    "client_token": "068030ce-849b-4f6d-bab1-ca42e929186a",
     "client_path_prefix" : "/client",
     "admin_path_prefix": "/admin"
 }
